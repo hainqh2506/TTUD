f3997a8c5206ca744321f2c0b208f5a2da51b056
tuananh2k11 <86148934+tuananh2k11@users.noreply.github.com>
2023-12-24 16:06:52 +0700

    Create bai1giai.cpp

---
+++ thi gk 2023_1/bai1giai.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+const int MOD = 1000000007;
+
+int countPairs(int n, const vector<int>& arr) {
+    unordered_map<int, int> freq;
+    long long result = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int x = arr[i];
+        result = (result + freq[x - 1]) % MOD;
+        freq[x]++;
+    }
+
+    return static_cast<int>(result);
+}
+
+int main() {
+    // Đọc dữ liệu đầu vào
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    // Tính và in ra kết quả
+    int result = countPairs(n, arr);
+    cout << result << endl;
+
+    return 0;
+}
