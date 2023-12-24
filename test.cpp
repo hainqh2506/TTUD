ef343b4b720f800561d8505b61462d45bf3c4e76
hainqh2506 <haiatbn123@gmail.com>
2023-12-24 16:19:16 +0700

    help

--- test.cpp
+++ test.cpp
@@ -1,7 +1,34 @@
+// bai 1 test sdfsd
 #include <bits/stdc++.h>
-#define N 100
 using namespace std;
+#define N 1000005
+long long Q = 1e9 + 7;
+
+using namespace std;
+
 int main()
 {
+    int a[N];
+    int n;
+    int cnt = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            if (a[i] == a[j] + 1 || a[i] == a[j] - 1)
+            {
+                cnt++;
+            }
+        }
+    }
+    int res;
+    res = cnt / Q;
+    printf("%d", res);
+
     return 0;
 }
--- thi gk 2023_1/test1.cpp
+++ thi gk 2023_1/test1.cpp
@@ -1,11 +1,32 @@
-// bai 1 test sdfsd
+
 #include <bits/stdc++.h>
-#define N 100
+#define N 1000005
+long long Q = 1e9 + 7;
 int a[N];
-int n;
+unsigned int n;
+int count = 0;
 using namespace std;
+
+void sol()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if(a[i]==a[j]+1 || a[i]==a[j]-1){
+                count ++;
+            }
+        }
+    }
+}
+
 int main()
-{   
-    
+{
+    scanf("%d", n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", a[i]);
+    }
+
     return 0;
 }
