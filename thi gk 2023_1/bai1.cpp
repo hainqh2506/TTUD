7b2abb21d1a523896b2b486b37140c1599d58155
hainqh2506 <haiatbn123@gmail.com>
2023-12-24 15:42:53 +0700

    k

---
+++ thi gk 2023_1/bai1.cpp
@@ -0,0 +1,23 @@
+/*bai1 : cho dãy số nguyên a[1], a[2],..,a[n]. hãy đếm Q số các cặp 2 chỉ số khác  i và j trong dãy sao cho hai phần tử a[i]và a[j] chênh lệch nhau 1 đơn vị
+ và 1<=i<j<=n 
+ input:
+ dòng 1: chứa số nguyên dương n (1 <=n <= 10000)
+ dòng 2: chứa n số nguyên a[1],a[2],...,a[n] (1<= a[i]<1000000)
+ kết quả:
+ ghi ra 1 số nguyên duy nhất là số dư của Q khi chia cho 10^9 +7
+ ví dụ:input: 
+ 5
+ 5 2 5 3 2
+ output:
+ 2
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+#define N 10005
+
+
+int main(){
+    return 0;
+
+}
--- thi gk 2023_1/test1.cpp
+++ thi gk 2023_1/test1.cpp
@@ -1,8 +1,11 @@
 // bai 1 test sdfsd
 #include <bits/stdc++.h>
 #define N 100
+int a[N];
+int n;
 using namespace std;
 int main()
-{
+{   
+    
     return 0;
 }
